e0ce22e1c1d7ae7787c823f251887eacad7bce3f
nnazo <jacobcurtis2266@gmail.com>
2020-12-10 15:32:04 -0600

    refactor error response

--- source-code-parser-web/src/routes.rs
+++ source-code-parser-web/src/routes.rs
@@ -5,6 +5,13 @@ use rust_code_analysis::*;
 use source_code_parser::*;
 use std::io::{BufReader, Read};
 
+fn error_response(reason: &str) -> JsonValue {
+    return json!({
+        "status": "error",
+        "reason": reason
+    });
+}
+
 /// Endpoint for retrieving the AST of a source code file supported by the `rust_code_analysis` crate.
 /// The POST request's Content-Type header should be multipart/form-data and should contain two
 /// entries: one with key "file" with the file data, and another with the key "ext" with the
@@ -12,19 +19,13 @@ use std::io::{BufReader, Read};
 #[post("/ast", data = "<file>")]
 pub fn ast(cont_type: &ContentType, file: Data) -> JsonValue {
     if !cont_type.is_form_data() {
-        return json!({
-            "status": "error",
-            "reason": "Content-Type header must be multipart/form-data"
-        });
+        return error_response("Content-Type header must be multipart/form-data");
     }
 
     let (_, boundary) = match cont_type.params().find(|&(k, _)| k == "boundary") {
         Some(x) => x,
         None => {
-            return json!({
-                "status": "error",
-                "reason": "Error multipart/form-data: boundary parameter not provided"
-            });
+            return error_response("Error multipart/form-data: boundary parameter not provided");
         }
     };
     let mut code = String::new();
@@ -40,26 +41,20 @@ pub fn ast(cont_type: &ContentType, file: Data) -> JsonValue {
 
                     if &entry.headers.name.to_string() == "file" {
                         if let Err(err) = reader.read_to_string(&mut code) {
-                            return json!({
-                                "status": "error",
-                                "reason": format!("Error reading form data {:?}", err)
-                            });
+                            let reason = format!("Error reading form data {:?}", err);
+                            return error_response(&reason);
                         }
                     } else if &entry.headers.name.to_string() == "ext" {
                         if let Err(err) = reader.read_to_string(&mut ext) {
-                            return json!({
-                                "status": "error",
-                                "reason": format!("Error reading form data {:?}", err)
-                            });
+                            let reason = format!("Error reading form data {:?}", err);
+                            return error_response(&reason);
                         }
                     }
                 }
             }
             Err(err) => {
-                return json!({
-                    "status": "error",
-                    "reason": format!("Error multipart/form-data: Could not read field {}", err)
-                });
+                let reason = format!("Error multipart/form-data: Could not read field {}", err);
+                return error_response(&reason);
             }
         }
     }
@@ -75,17 +70,11 @@ pub fn ast(cont_type: &ContentType, file: Data) -> JsonValue {
         span,
     }) {
         Some(ast) => json!(ast),
-        None => json!({
-            "status": "error",
-            "reason": "Could not parse source file."
-        }),
+        None => error_response("Could not parse source file."),
     }
 }
 
 #[catch(404)]
 pub fn not_found() -> JsonValue {
-    json!({
-        "status": "error",
-        "reason": "Resource was not found."
-    })
+    error_response("Resource was not found.")
 }
