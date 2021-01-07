4b2c05d6fa800fe6c6c798047af01ff2b3ab8bd9
nnazo <jacobcurtis2266@gmail.com>
2021-01-07 11:51:34 -0600

    change method parameter stereotype to field

--- src/lang/cpp.rs
+++ src/lang/cpp.rs
@@ -326,7 +326,7 @@ fn func_parameter(param_decl: &AST, module_name: &str, path: &str) -> Option<Met
             path: path.to_string(),
             package_name: module_name.to_string(),
             instance_name: ident.clone(),
-            instance_type: InstanceType::AnalysisComponent,
+            instance_type: InstanceType::FieldComponent,
         },
         annotation: None,
         parameter_name: ident,
