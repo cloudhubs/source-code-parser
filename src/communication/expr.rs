f8688dfd13e25dbc9cd4cd8461347c510fdac4c2
nnazo <jacobcurtis2266@gmail.com>
2021-03-31 14:39:59 -0500

    fix issue with finding definitions for functional ms

--- src/communication/expr.rs
+++ src/communication/expr.rs
@@ -34,7 +34,7 @@ impl CommunicationReplacer for CallExpr {
         callee_class: Option<&ClassOrInterfaceComponent>,
         callee_method: &MethodComponent,
     ) -> Option<Node> {
-        // TODO: convert call expressions that are REST or RPC calls.
+        // TODO: convert found call expressions that are REST or RPC calls.
 
         // Get name -- could be Ident, or DotExpr where the left hand is the identifier.
         // For REST and RPC calls, it should be a DotExpr most likely since it will be using
@@ -64,8 +64,8 @@ impl CommunicationReplacer for CallExpr {
 
         let client_ident = match_ident_or(&*client_call.expr)?.name.to_lowercase();
         let method_ident = match_ident_or(&*client_call.selected)?;
-        let client_name = get_service_name(callee_class?, &client_ident);
-        if let None = client_name {
+        let client_name = get_service_name(callee_class, &client_ident);
+        if client_name.is_none() {
             self.args.iter_mut().for_each(|arg| {
                 if let Some(Node::Expr(replacement)) =
                     arg.replace_communication_call(modules, module, callee_class, callee_method)
@@ -87,8 +87,13 @@ impl CommunicationReplacer for CallExpr {
                         if method.method_name == method_ident.name
                             && self.args.len() == method.parameters.len()
                             && callee_method.component.path != class.component.component.path
-                            && callee_class?.component.container_name
-                                != class.component.container_name
+                            && match callee_class {
+                                Some(callee_class) => {
+                                    callee_class.component.container_name
+                                        != class.component.container_name
+                                }
+                                _ => true,
+                            }
                         {
                             // Found it
                             result = Some(format!(
@@ -135,31 +140,36 @@ fn match_ident_or(expr: &Expr) -> Option<&Ident> {
     }
 }
 
-fn get_service_name(class: &ClassOrInterfaceComponent, client_ident: &str) -> Option<String> {
+fn get_service_name(
+    class: Option<&ClassOrInterfaceComponent>,
+    client_ident: &str,
+) -> Option<String> {
     // Search through the modules pertaining to the client using the client name
     let mut client_name = client_ident
         .replace("client", "")
         .replace("pool", "")
         .replace("_", "");
-    for field in class.field_components.iter() {
-        let field_name = if field.r#type != "" {
-            field.r#type.to_lowercase()
-        } else {
-            field.field_name.to_lowercase()
-        };
-        if field_name.contains("client") {
-            // println!("before first split {}", field_name);
-            let mut field_name = if field_name.contains("client>") {
-                field_name.split("client>").next()?
+    if let Some(class) = class {
+        for field in class.field_components.iter() {
+            let field_name = if field.r#type != "" {
+                field.r#type.to_lowercase()
             } else {
-                field_name.split("client").next()?
+                field.field_name.to_lowercase()
             };
-            if field_name.contains("<") {
-                // println!("before 2nd split {}", field_name);
-                field_name = field_name.split("<").last()?;
+            if field_name.contains("client") {
+                // println!("before first split {}", field_name);
+                let mut field_name = if field_name.contains("client>") {
+                    field_name.split("client>").next()?
+                } else {
+                    field_name.split("client").next()?
+                };
+                if field_name.contains("<") {
+                    // println!("before 2nd split {}", field_name);
+                    field_name = field_name.split("<").last()?;
+                }
+                // println!("{} -> {}", client_name, field_name);
+                client_name = field_name.replace("_", "").replace("service", "").into();
             }
-            // println!("{} -> {}", client_name, field_name);
-            client_name = field_name.replace("_", "").replace("service", "").into();
         }
     }
     Some(client_name)
