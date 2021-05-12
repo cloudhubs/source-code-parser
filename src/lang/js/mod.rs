7097deaf54de73ecc52bd6066ce5c9d8aac18f70
Vincent Bushong <vinbush@gmail.com>
2021-05-12 10:43:13 -0500

    start with js stuff

---
+++ src/lang/js/mod.rs
@@ -0,0 +1,10 @@
+use crate::parse::AST;
+use crate::prophet::*;
+
+pub fn find_components(ast: AST, path: &str) -> Vec<ComponentType> {
+    find_components_internal(ast, String::new(), path)
+}
+
+fn find_components_internal(ast: AST, mut package: String, path: &str) -> Vec<ComponentType> {
+    
+}
