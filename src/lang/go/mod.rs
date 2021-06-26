48ebdf2a757c1d83c11854e1f977f2df1f34f1ca
Alehman74 <Alehman74@yahoo.com>
2021-06-26 13:42:41 -0500

    Added go folder and updated mod.rs

---
+++ src/lang/go/mod.rs
@@ -0,0 +1,12 @@
+use crate::parse::AST;
+use crate::prophet::*;
+
+/*
+pub fn find_components(ast: AST, path: &str) -> Vec<ComponentType> {
+    find_components_internal(ast, String::new(), path)
+}
+
+pub fn find_components_internal(ast: AST, mut package: String, path: &str) -> Vec<ComponentType> {
+
+}
+*/
