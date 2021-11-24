cdd61e25815059247f35059106d1eed80a4678ee
nnazo <jacobcurtis2266@gmail.com>
2021-11-24 13:40:00 -0600

    convert extraction macros to higher order functions

--- src/ressa/result.rs
+++ src/ressa/result.rs
@@ -1,6 +1,6 @@
 use std::collections::{BTreeMap, HashMap};
 
-use runestick::{Object, Value};
+use runestick::{Object, Shared, Value};
 
 /// Output type from the library
 pub type RessaResult = HashMap<String, BTreeMap<String, Value>>;
@@ -14,48 +14,54 @@ pub enum Error {
     RuneAccess(runestick::AccessError),
 }
 
-#[macro_export]
-macro_rules! coerce {
-    ( $value:tt, $target:tt ) => {
-        match $value.clone().$target() {
-            ::std::result::Ok(obj) => match obj.take() {
-                ::std::result::Ok(obj) => ::std::result::Ok(obj),
-                ::std::result::Err(err) => {
-                    ::std::result::Err($crate::ressa::result::Error::RuneAccess(err))
-                }
-            },
-            ::std::result::Err(_) => {
-                ::std::result::Err($crate::ressa::result::Error::RuneAcquisition)
-            }
-        }
-    };
+/// Coerces a `runestick::Value` into a `Shared<T>`
+pub fn coerce<I, T, E>(value: &Value, into: I) -> Result<T, Error>
+where
+    I: FnOnce(Value) -> Result<Shared<T>, E>,
+{
+    match into(value.clone()) {
+        Ok(obj) => obj.take().map_err(Error::RuneAccess),
+        Err(_) => Err(Error::RuneAcquisition),
+    }
 }
 
-#[macro_export]
-macro_rules! extract {
-    ( $value:ident, $name:tt, $target:tt ) => {
-        match $value.get($name) {
-            ::std::result::Some(obj) => coerce!(obj, $target),
-            ::std::result::None => ::std::result::Err(
-                $crate::ressa::result::Error::Error::MissingKey($name.to_string()),
-            ),
-        }
-    };
+/// Extracts a value from the given object at key `name` and coerces it into `T`
+pub fn extract<I, T, E>(obj: &BTreeMap<String, Value>, name: &str, into: I) -> Result<T, Error>
+where
+    I: FnOnce(Value) -> Result<Shared<T>, E>,
+{
+    obj.get(name)
+        .ok_or_else(|| Error::MissingKey(name.to_string()))
+        .and_then(|value| coerce(value, into))
 }
 
+/// Coerces a `runestick::Object` into a `BTreeMap<String, Value>`
 pub fn extract_object(obj: Object) -> BTreeMap<String, Value> {
     obj.into_inner()
 }
 
-#[macro_export]
-macro_rules! extract_vec {
-    ( $value:ident, $name:tt, $target:tt ) => {{
-        let mut converted = std::vec![];
-        for elem in extract!($value, $name, into_vec)?.iter() {
-            converted.push(coerce!(elem, $target)?);
-        }
-        ::std::result::Ok(converted)
-    }};
+/// Gets the `runestick::Vec` at key `name` and coerces it into a `Vec<T>`
+pub fn extract_vec<I, T, E>(
+    obj: &BTreeMap<String, Value>,
+    name: &str,
+    into: I,
+) -> Result<Vec<T>, Error>
+where
+    I: Fn(Value) -> Result<Shared<T>, E> + Copy,
+{
+    let converted = extract(obj, name, Value::into_vec)?
+        .into_iter()
+        // `into` has the Copy trait bound so it can be called multiple times when
+        // captured by map's `FnMut` argument
+        .map(|elem| coerce(&elem, into));
+
+    // Short-circuit any errors that occurred
+    if let Some(bad_result) = converted.clone().find(Result::is_err) {
+        bad_result?;
+    }
+
+    // Coerce results out of the vec
+    Ok(converted.flatten().collect())
 }
 
 /// Retrieve an object from the map, returning it or an error indicating a missing key
