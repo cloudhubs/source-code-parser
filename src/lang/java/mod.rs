7228e5084b3ddd8993c42da4076462f815a132db
MicahSchiewe <60760216+MicahSchiewe@users.noreply.github.com>
2022-01-07 13:39:57 -0600

    Sort files (#52)
    * Add package matching to ClassOrInterfaceComponent
    
    * Update recorded results
    
    * Add Postman tests for reproducibility
    
    * Update ressa_specification.md
    
    * Update to finish fixing TrainTicket ReSSAs
    
    * Add train-ticket ground truths
    
    * update ressas in benchmark constants
    
    * remove java package debug print
    
    * updat benchmarks
    
    * clarify linux deps for benchmarks
    
    * add note about jemalloc builds
    
    * Linting
    
    Co-authored-by: nnazo <jacobcurtis2266@gmail.com>

--- src/lang/java/mod.rs
+++ src/lang/java/mod.rs
@@ -40,13 +40,18 @@ pub fn find_components(ast: AST, path: &str) -> Vec<ComponentType> {
     find_components_internal(ast, String::new(), path)
 }
 
-fn find_components_internal(ast: AST, package: String, path: &str) -> Vec<ComponentType> {
+fn find_components_internal(ast: AST, mut package: String, path: &str) -> Vec<ComponentType> {
     let mut components = vec![];
 
+    // Find package declaration
+    if let Some(node) = ast.find_child_by_type(&["package_declaration"]) {
+        package = parse_package(node);
+    }
+
+    // Parse rest of object
     for node in ast
         .find_all_children_by_type(&[
-            "import_declaration",
-            "package_declaration",
+            // "import_declaration",
             "class_declaration",
             "interface_declaration",
             "enum_declaration",
@@ -55,57 +60,42 @@ fn find_components_internal(ast: AST, package: String, path: &str) -> Vec<Compon
         .get_or_insert(vec![])
         .iter()
     {
-        match &*node.r#type {
-            "import_declaration" => tracing::info!("{}", parse_import(node)),
-            "package_declaration" => {
-                // package = parse_package(&node)
-                //     .expect(&*format!("Malformed package declaration {:#?}!", node));
-                // tracing::info!("{}", package);
+        if let Some(class) = parse_class(node, &*package, path) {
+            // Save the methods
+            for method in class.component.methods.iter() {
+                components.push(ComponentType::MethodComponent(method.clone()));
             }
-            "class_declaration"
-            | "interface_declaration"
-            | "enum_declaration"
-            | "annotation_declaration" => {
-                if let Some(class) = parse_class(node, &*package, path) {
-                    // Save the methods
-                    for method in class.component.methods.iter() {
-                        components.push(ComponentType::MethodComponent(method.clone()));
-                    }
-                    // class.component.methods = vec![];
 
-                    // Save the class itself
-                    components.push(ComponentType::ClassOrInterfaceComponent(class));
-                }
-            }
-            tag => todo!("Cannot identify provided tag {:#?}", tag),
-        };
+            // Save the class itself
+            components.push(ComponentType::ClassOrInterfaceComponent(class));
+        }
     }
 
     components
 }
 
 /// Take in the AST node containing the package declaration, and--if it is not malformed--return a string representing the package
-// fn parse_package(ast: &AST) -> Option<String> {
-//     let result = ast.find_child_by_type(&["identifier", "scoped_identifier"])?;
-//     let mut buffer = String::new();
-//     for member in result.children.iter() {
-//         if member.r#type == "scoped_identifier" {
-//             buffer = format!("{}{}", parse_package(result)?, buffer);
-//         } else {
-//             buffer.push_str(&*member.value);
-//         }
-//     }
-//     Some(buffer)
-// }
-
-/// Take the AST node containing an import statement, and return back the String describing the package imported
-fn parse_import(ast: &AST) -> String {
-    let mut buffer = String::new();
-    for child in ast.children.iter() {
-        match &*child.r#type {
-            "identifier" | "." | "*" => buffer.push_str(&*child.value),
-            _ => buffer.push_str(&*parse_import(child)),
-        };
-    }
-    buffer
+fn parse_package(ast: &AST) -> String {
+    ast.children.iter().map(do_parse_package_node).collect()
 }
+
+/// Parse an individual subnode of a package declaration
+fn do_parse_package_node(ast: &AST) -> String {
+    match &*ast.r#type {
+        "identifier" | "." => ast.value.clone(),
+        "scoped_identifier" => parse_package(ast),
+        _ => String::new(),
+    }
+}
+
+// /// Take the AST node containing an import statement, and return back the String describing the package imported
+// fn parse_import(ast: &AST) -> String {
+//     let mut buffer = String::new();
+//     for child in ast.children.iter() {
+//         match &*child.r#type {
+//             "identifier" | "." | "*" => buffer.push_str(&*child.value),
+//             _ => buffer.push_str(&*parse_import(child)),
+//         };
+//     }
+//     buffer
+// }
