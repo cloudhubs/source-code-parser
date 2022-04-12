def910c9e03632728b086359ceac2f20805974b2
nnazo <jacobcurtis2266@gmail.com>
2022-04-11 21:42:39 -0500

    working on relational and structural constraints for z3

--- src/ressa/constraints/check.rs
+++ src/ressa/constraints/check.rs
@@ -1,5 +1,3 @@
-use std::any::Any;
-
 use super::{
     Constraint, ConstraintLogic, ConstraintTree, MethodConstraint, RelationalConstraint,
     StructuralConstraint,
@@ -42,23 +40,30 @@ pub fn check(to_match: &Constraint, candidate: &[&Constraint]) -> bool {
     true
 }
 
-fn convert_constraint(
+fn add_constraint(
     ctx: &'static Context,
     solver: &'static Solver,
     constraint: &ConstraintTree,
 ) -> Option<Box<dyn AstExt>> {
     use ConstraintTree::*;
     match constraint {
-        VariableConstraint(ident) => Some(Box::new(Bool::new_const(ctx, ident.clone()))),
-        MethodConstraint(method) => convert_method_constraint(ctx, solver, method),
-        RelationalConstraint(relation) => convert_relational_constraint(ctx, solver, relation),
-        StructuralConstraint(structural) => convert_structural_constraint(ctx, solver, structural),
-        LiteralConstraint(lit) => Some(Box::new(ast::String::new_const(ctx, lit.clone()))),
-        _ => None,
+        VariableConstraint(ident) => add_variable_constraint(ctx, solver, ident), //Some(Box::new(Bool::new_const(ctx, ident.clone()))),
+        MethodConstraint(method) => add_method_constraint(ctx, solver, method),
+        RelationalConstraint(relation) => add_relational_constraint(ctx, solver, relation),
+        StructuralConstraint(structural) => add_structural_constraint(ctx, solver, structural),
+        LiteralConstraint(lit) => add_literal_constraint(ctx, solver, lit), //Some(Box::new(ast::String::new_const(ctx, lit.clone()))),
     }
 }
 
-fn convert_method_constraint(
+fn add_variable_constraint(
+    ctx: &'static Context,
+    solver: &'static Solver,
+    constraint: &str,
+) -> Option<Box<dyn AstExt>> {
+    None
+}
+
+fn add_method_constraint(
     ctx: &'static Context,
     solver: &'static Solver,
     constraint: &MethodConstraint,
@@ -66,18 +71,17 @@ fn convert_method_constraint(
     None
 }
 
-fn convert_relational_constraint(
+fn add_relational_constraint(
     ctx: &'static Context,
     solver: &'static Solver,
     constraint: &RelationalConstraint,
 ) -> Option<Box<dyn AstExt>> {
-    use ConstraintLogic::*;
-
-    let lhs = convert_constraint(ctx, solver, &constraint.lhs)?;
-    let rhs = convert_constraint(ctx, solver, &constraint.rhs)?;
+    let lhs = add_constraint(ctx, solver, &constraint.lhs)?;
+    let rhs = add_constraint(ctx, solver, &constraint.rhs)?;
 
     match (lhs.get_sort().kind(), rhs.get_sort().kind()) {
         (SortKind::Int, SortKind::Int) => {
+            use ConstraintLogic::*;
             let lhs_int = lhs.downcast_ref::<ast::Int>()?;
             let rhs_int = lhs.downcast_ref::<ast::Int>()?;
             let c = match constraint.r#type {
@@ -88,18 +92,77 @@ fn convert_relational_constraint(
                 GreaterThan => lhs_int.gt(rhs_int),
                 GreaterThanEqualTo => lhs_int.ge(rhs_int),
             };
-            solver.assert(&c);
+            // solver.assert(&c);
+            Some(Box::new(c))
         }
-        _ => {}
-    };
-
-    None
+        (lhs_sort, rhs_sort) => {
+            println!("Sort: {lhs_sort:?}, {rhs_sort:?}");
+            None
+        }
+    }
 }
 
-fn convert_structural_constraint(
+fn add_structural_constraint(
     ctx: &'static Context,
     solver: &'static Solver,
     constraint: &StructuralConstraint,
+) -> Option<Box<dyn AstExt>> {
+    let z3_children: Vec<_> = constraint
+        .children
+        .iter()
+        .flat_map(|child| add_constraint(ctx, solver, child))
+        .collect();
+
+    use super::ConstraintComposition::*;
+    match constraint.r#type {
+        And | Or => {
+            let bool_children: Vec<_> = z3_children
+                .iter()
+                .flat_map(|c| c.downcast_ref::<ast::Bool>())
+                .collect();
+            let op = match &constraint.r#type {
+                And => ast::Bool::and,
+                Or => ast::Bool::or,
+                _ => unreachable!(),
+            };
+            Some(Box::new(op(ctx, &bool_children)))
+        }
+        Not => match z3_children.first() {
+            Some(child) => {
+                let bool_child = child.downcast_ref::<ast::Bool>()?;
+                Some(Box::new(bool_child.not()))
+            }
+            _ => None,
+        },
+        Plus | Minus | Multiply => {
+            // I just realized I don't think I can cast the children to ints
+            // since practically thing will end up as a tree of children
+            // realistically rather than a linear list of the ints..
+            let int_children: Vec<_> = z3_children
+                .iter()
+                .flat_map(|c| c.downcast_ref::<ast::Int>())
+                .collect();
+            let op = match &constraint.r#type {
+                Plus => ast::Int::add,
+                Minus => ast::Int::sub,
+                Multiply => ast::Int::mul,
+                _ => unreachable!(),
+            };
+            Some(Box::new(op(ctx, &int_children)))
+        }
+        Dot => {
+            let _ = 0;
+            None
+        }
+        // Ignoring Shifts because Z3 ints do not support them
+        _ => None,
+    }
+}
+
+fn add_literal_constraint(
+    ctx: &'static Context,
+    solver: &'static Solver,
+    constraint: &str,
 ) -> Option<Box<dyn AstExt>> {
     None
 }
